f7040694f6ff352bdd12d8a9281a9491a6aaf7d5
HIRAISHI Tasuku <hiraishi.tasuku.h04@kyoto-u.jp>
2022-05-27 15:07:02 -0700

    Updated Cprograms.

--- sort-base.c
+++ sort-base.c
@@ -2,6 +2,22 @@
 #include<stdlib.h>
 #include<time.h>
 
+//////////////////////////////////////////////////
+// 課題8: バブルソート
+// num[0]~nums[nelm-1]を昇順に並べ替える
+int bubble_sort (int nums[], int nelm)
+{
+  // ここを演習で作成する
+}
+
+// 課題9: クイックソート
+// num[0]~nums[nelm-1]を昇順に並べ替える
+int quick_sort (int nums[], int nelm)
+{
+  // ここを演習で作成する
+}
+//////////////////////////////////////////////////
+
 // 現在時刻を取得する．精度はナノ秒（ns），返す値の単位は秒（s）
 double get_current_time (void) {
   struct timespec tp;
@@ -18,21 +34,14 @@ void print_nums (int *nums, int nelm)
   printf ("\n");
 }
 
-// 課題8: バブルソート
-// num[0]~nums[nelm-1]を昇順に並び換える
-int bubble_sort (int nums[], int nelm)
-{
-  // ここを演習で作成する
-}
-
-// 課題9: クイックソート
-// num[0]~nums[nelm-1]を昇順に並び換える
-int quick_sort (int nums[], int nelm)
-{
-  // ここを演習で作成する
-}
-
-int main (void) {
+int main (int argc, char *argv[]) {
+  // バブルソートとクイックソートのどちらを使うかを選択
+  int al;  // al==1ならバブルソート，al==2ならクイックソート
+  if (argc<1) {
+    al = 1;
+  } else {
+    al = atoi (argv[1]);   // atoiは文字列を数値に変替する関数
+  }
   // 標準入力から数列を読み込む
   int nelm=0;        // 配列の要素数 
   int *nums;         // 配列の先頭要素へのポインタ
@@ -50,17 +59,30 @@ int main (void) {
     nums[nelm] = n;
     nelm++;
   }
-  printf ("読み込み終了: 要素数は%d\n", nelm);
+  printf ("数列読み込み終了: 要素数は%d\n", nelm);
   // この下の行の//を外すと、読み込み後に数列を表示
   // print_nums(nums, nelm);
 
-  printf ("数列を昇順に並び換えます\n");
-  double t1 = get_current_time();  // 開始時刻
+  printf ("数列を昇順に並べ替えます\n");
+  double t1;                        // 開始時刻
 
   //// 重要：この時点で，nelmには要素の数，nums[0]~nums[n-1]には読み込んだ数列が入っている
-  // 重要：下2行のうちどちらかのみを//を置かずに残す
-  bubble_sort (nums, nelm);
-  // quick_sort (nums, nelm);
+  switch (al) {
+  case 1:
+    printf ("バブルソートで並べ替え\n");
+    t1 = get_current_time();        // 時間計測開始
+    bubble_sort (nums, nelm);
+    break;
+  case 2:
+    printf ("クイックソートで並べ替え\n");
+    t1 = get_current_time();        // 時間計測開始
+    quick_sort (nums, nelm);
+    break;
+  default:
+    printf ("不正なアルゴリズム番号です\n");
+    break;
+  }
+  // 重要：この時点で，nums[0]~nums[n-1]の並べ替えが完了している
 
   double t2 = get_current_time();  // 終了時刻
   print_nums(nums, nelm);
